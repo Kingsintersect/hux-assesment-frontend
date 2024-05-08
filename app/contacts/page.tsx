00a2ec32ece820c29dd5e71a4a43c58c94ff317a
Kingsintersect <kingsintersect@gmail.com>
2024-05-08 06:43:31 -0700

    REVERTED BACK TO "did the same for both create and edit forms"=>RESPONSIVENESS: Implemented responsive behavior to all my pages

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -5,11 +5,15 @@ import ContactListTable from "../ui/tables/ContactListTable";
 const ContactsPage = () => {
     return (
         <>
-            <section className="max-w-md mx-auto  rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-24">
-                <div className="flex flex-col items-center justify-start px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <Suspense key={'query' + 'currentPage'} fallback={<div>Loading</div>}>
-                        <ContactListTable />
-                    </Suspense>
+            <section className="max-w-md mx-auto md:max-w-2xl sm:max-w-xl mt-24">
+                <div className="mx-5">
+                    <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto rounded-2xl shadow-sm shadow-orange-400">
+                        <div className="w-full py-20 px-10">
+                            <Suspense key={'query' + 'currentPage'} fallback={<div>Loading...</div>}>
+                                <ContactListTable />
+                            </Suspense>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
