380be1b585237359c4171d96086c19b8c0f871dd
Kingsintersect <kingsintersect@gmail.com>
2024-05-08 07:37:47 -0700

    changed the phone number pattern to match local nigerian parttern. did yhe same for edit contact and create contact

--- app/ui/contacts/EditContactForm.tsx
+++ app/ui/contacts/EditContactForm.tsx
@@ -100,10 +100,10 @@ export default function EditInvoiceForm({ contactId }: { contactId: string; }) {
             </div>
             <div className="relative z-0 w-full mb-5 group">
                 <input value={contact?.phoneNumber}
-                    type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                    type="tel" pattern="[0-9]-{11}"
                     {...register('phoneNumber')}
                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                <label htmlFor="phoneNumber" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
+                <label htmlFor="phoneNumber" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number <span className="text-orange-500">(08012345689)</span></label>
                 {errors.phoneNumber && <p className='text-red-200 text-sm font-semi-bold'>{errors.phoneNumber.message}</p>}
             </div>
             {error && <p className='text-red-200 font-semibold my-3'>{error}</p>}
